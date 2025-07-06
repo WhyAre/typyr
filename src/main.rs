2de0b461ae6d75b12c054d37f7cac904ae56b5f9
WhyAre <laiyr52@gmail.com>
2025-07-06 19:59:17 +0800

    working

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
+use crossterm::cursor::MoveTo;
+use crossterm::execute;
+use crossterm::terminal::{Clear, ClearType};
+use itertools::Itertools;
+use portable_pty::{Child, CommandBuilder, PtySize, native_pty_system};
 use std::fs::OpenOptions;
 use std::io::{self, Read, Write};
+use std::process::Command;
 use std::thread;
-
-use crossterm::cursor::{MoveTo, MoveUp, RestorePosition, SavePosition};
-use crossterm::execute;
-use crossterm::style::{Print, ResetColor};
-use crossterm::terminal::{
-    Clear, ClearType, ScrollDown, ScrollUp, SetSize, WindowSize, window_size,
-};
-use portable_pty::{Child, CommandBuilder, PtySize, native_pty_system};
 
 use anyhow::Result;
 
+/// Just write enough code until this is correct... please tell me if there's a library for this
+fn pretty_display(code: &u8) -> String {
+    match code {
+        1..=26 => format!("<C-{}>", (b'A' + code - 1) as char),
+        27 => "<Esc>".to_string(),
+        32..=126 => (*code as char).to_string(), // printable ASCII
+        127 => "<BS>".to_string(),
+        _ => format!("<0x{code:02X}>"),
+    }
+}
+
 #[allow(clippy::type_complexity)]
-fn spawn_shell() -> Result<(
+fn spawn_shell(
+    cmd: &str,
+) -> Result<(
     Box<dyn Child + Send + Sync + 'static>,
     Box<dyn Read + Send + 'static>,
     Box<dyn Write + Send + 'static>,
@@ -35,7 +46,7 @@ fn spawn_shell() -> Result<(
     })?;
 
     // Spawn a shell into the pty
-    let cmd = CommandBuilder::new("tmux");
+    let cmd = CommandBuilder::new(cmd);
     let child = pair.slave.spawn_command(cmd)?;
 
     // Read and parse output from the pty with reader
@@ -48,9 +59,11 @@ fn spawn_shell() -> Result<(
 }
 
 fn main() -> Result<()> {
+    let cmd = std::env::args().nth(1).expect("missing argument");
+
     crossterm::terminal::enable_raw_mode()?;
 
-    let (mut child, mut reader, mut writer) = spawn_shell()?;
+    let (mut child, mut reader, mut writer) = spawn_shell(&cmd)?;
 
     // let WindowSize { rows, columns, .. } = window_size()?;
 
@@ -64,6 +77,7 @@ fn main() -> Result<()> {
             .create(true)
             .open("/tmp/file.txt")
             .unwrap();
+        let mut tmux_refresh = Command::new("tmux");
 
         let stdin = io::stdin();
         let mut handle = stdin.lock();
@@ -75,8 +89,15 @@ fn main() -> Result<()> {
             writer.write_all(&buf[..n]).unwrap();
             writer.flush().unwrap();
 
-            write!(file, "{:?}", &buf[..n]).unwrap();
+            let out_str: String = buf[..n].iter().map(pretty_display).collect();
+            write!(file, "{out_str}").unwrap();
             file.flush().unwrap();
+
+            tmux_refresh
+                .arg("refresh-client")
+                .arg("-S")
+                .output()
+                .unwrap();
         }
     });
 
@@ -91,17 +112,6 @@ fn main() -> Result<()> {
 
             stdout.write_all(&buf[..n]).unwrap();
             stdout.flush().unwrap();
-
-            // execute!(
-            //     stdout,
-            //     // SavePosition,
-            //     // MoveTo(0, rows - 1), // Move to bottom-left
-            //     // ResetColor,
-            //     // Clear(ClearType::CurrentLine),
-            //     // Print("Y".repeat(columns as usize)),
-            //     // RestorePosition,
-            // )
-            // .unwrap();
         }
     });
 
